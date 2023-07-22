bb1f7b5f77d6384716232e124feed8af0262f1ee
Daniil Frolov <frolofelo@gmail.com>
2023-07-22 14:51:50 +0300

    Клавиатура главного меню

--- keyboards.py
+++ keyboards.py
@@ -1,12 +1,11 @@
 from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
 
-MENU_BUTTONS = ['Запись✅', 'Отмена записи❌', 'Мои записи📝']
-
 
 def create_markup_menu():
+    menu_buttons = ['Запись✅', 'Отмена записи❌', 'Мои записи📝']
     markup = InlineKeyboardMarkup(row_width=2)
-    markup.add(InlineKeyboardButton(text=MENU_BUTTONS[0], callback_data='RECORD'))
-    markup.add(InlineKeyboardButton(text=MENU_BUTTONS[1], callback_data='CANCEL_RECORD'))
-    markup.add(InlineKeyboardButton(text=MENU_BUTTONS[2], callback_data='MY_RECORD'))
+    markup.add(InlineKeyboardButton(text=menu_buttons[0], callback_data='RECORD'))
+    markup.add(InlineKeyboardButton(text=menu_buttons[1], callback_data='CANCEL_RECORD'))
+    markup.add(InlineKeyboardButton(text=menu_buttons[2], callback_data='MY_RECORD'))
 
     return markup
